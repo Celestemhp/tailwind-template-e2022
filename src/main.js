a013ee16af2054b5d1d0288c8e117dbf8ab25796
Celeste Michelle <cele0216@stud.kea@purplegraphics.dk>
2023-03-01 22:13:09 +0100

    foerste

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+// import { inView, animate } from "motion";
+
+// const inviewDesktop = () => {
+//   return inView(".viewinsection6", () => {
+//     animate(".section1-element1", { x: [-2000, -200] }, { duration: 2 });
+//     return () => {
+//      animate(".section1-element1", { x: [-2000, -200] }, { duration: 2 });
+//     };
+//   });
+// };
